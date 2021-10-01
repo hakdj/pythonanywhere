4415628fa9a328b88c42625ed5998b67a476b791
hakdj <hakdjhakdj@naver.com>
2021-10-01 10:57:42 +0900

    1001_test

---
+++ home/views.py
@@ -0,0 +1,56 @@
+import sqlite3
+from django.shortcuts import render
+from django.core.paginator import Paginator
+
+# Create your views here.
+def home(request):
+    result={}
+    return render(request, 'home.html', context=result)
+
+def index(request):
+    result={}
+    return render(request, 'index.html', context=result)
+
+def test(request):
+    result={}
+    return render(request, 'test.html', context=result)
+
+def premium(request):
+    result = dict()
+    conn = sqlite3.connect('db.sqlite3')
+    conn.row_factory = sqlite3.Row  # for getting columns
+    curs = conn.cursor()
+    # economics
+    curs.execute('select * from recipe_tot_youtube')
+    data = curs.fetchall()
+    for row in data:
+        print(row['title'], ' : ', row['method'])
+    paginator = Paginator(data, 5)
+    result = dict()
+    result['paginator'] = paginator
+    # request.GET['page']
+    page_number = request.GET.get('page', 1)
+    result['page_obj'] = paginator.get_page(page_number)
+    return render(request, 'premium.html', context=result)
+
+def frame_test(request):
+    result = dict()
+    conn = sqlite3.connect('db.sqlite3')
+    conn.row_factory = sqlite3.Row  # for getting columns
+    curs = conn.cursor()
+    # economics
+    curs.execute('select * from recipe_tot_youtube')
+    data = curs.fetchall()
+    for row in data:
+        print(row['title'], ' : ', row['method'])
+    paginator = Paginator(data, 5)
+    result = dict()
+    result['paginator'] = paginator
+    # request.GET['page']
+    page_number = request.GET.get('page', 1)
+    result['page_obj'] = paginator.get_page(page_number)
+    return render(request, 'frame_test.html', context=result)
+
+
+
+
